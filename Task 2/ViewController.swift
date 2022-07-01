ce72962446affcaeac8bcd8ad7d468fb5d321902
Tejasvni Dhir <tjdhir119@gmail.com>
2022-07-01 21:28:27 +0530

    Creating UI for login screen

--- Task 2/ViewController.swift
+++ Task 2/ViewController.swift
@@ -10,12 +10,20 @@ import GoogleSignIn
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //GIDSignIn.sharedInstance()?.presentingViewController = self
     }
 
-
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func facebookButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func googleButtonPressed(_ sender: UIButton) {
+    }
 }
 
