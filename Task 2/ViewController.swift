6a11833b71f84a0a4b0e377621cafca2132669a6
Tejasvni Dhir <tjdhir119@gmail.com>
2022-07-01 19:10:08 +0530

    Intial Commit

---
+++ Task 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 2
+//
+//  Created by Tejasvni Dhir on 01/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
